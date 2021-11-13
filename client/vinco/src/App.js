6f3a70fd73d04b9372dc78121c0a95bfbb6a32a7
urisk <carst333@gmail.com>
2021-11-13 11:33:28 +0100

    Add and fix funcionality in App

--- client/vinco/src/App.js
+++ client/vinco/src/App.js
@@ -1,8 +1,46 @@
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import ApiService from './Services/ApiServices';
 import Dashboard from './Components/Dashboard/Dashboard';
 import NavBar from './Components/Navbar/NavBar';
 
 function App() {
+
+  const [collectionListRecs, setCollectionRecs] = useState([]);
+  const [wishlistListRecs, setWishlistRecs] = useState([]);
+
+  useEffect(() => {
+      
+    ApiService.getAllFromCollection()
+        .then(collectionRecs => {
+          console.log('records', collectionRecs)
+          collectionRecs
+            .sort(function (a, b) {
+              if (a.artist < b.artist) return -1;
+              if (a.artist > b.artist) return 1;
+              return 0;
+            });
+            setCollectionRecs(collectionListRecs);
+        });
+    }, []);
+
+    useEffect(() => {
+      
+      ApiService.getAllFromWishlist()
+          .then(wishlistRecs => {
+            wishlistRecs
+              .sort(function (a, b) {
+                if (a.artist < b.artist) return -1;
+                if (a.artist > b.artist) return 1;
+                return 0;
+              });
+                console.log('records', wishlistRecs)
+              setWishlistRecs(wishlistListRecs);
+          });
+      }, []);
+
+
   return (
     <div className="App">
       <NavBar />
---
+++ client/vinco/src/Components/CollectionList/Collection.js
@@ -0,0 +1,12 @@
+import './Collection.css';
+import React from 'react';
+
+function Collection () {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Collection;
